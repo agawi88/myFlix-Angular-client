271b264a8ed760c22690295036bed189fa9e5500
Agnieszka <agnessn@icloud.com>
2025-12-05 14:02:04 +0100

    updated files
    deleted everything to get rid of SSR not necessary for the project

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { platformBrowser } from '@angular/platform-browser';
 import { AppModule } from './app/app-module';
+import { provideAnimations } from '@angular/platform-browser/animations';
+
 
 platformBrowser().bootstrapModule(AppModule, {
   ngZoneEventCoalescing: true,
