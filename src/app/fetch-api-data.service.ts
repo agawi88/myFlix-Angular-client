0fac9437d1a0f11a1ed27df729b5fb5e44adae7c
Agnieszka <agnessn@icloud.com>
2025-12-08 23:07:28 +0100

    updated file
    deleted comment

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -2,8 +2,6 @@ import { Injectable } from '@angular/core';
 import { catchError, map } from 'rxjs/operators';
 import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
-//import { map } from 'rxjs/operators';
-
 
 // Declaring the api url that will provide data for the client app
 // Use the Angular environment files when running with the Angular CLI (ng serve / ng build)
