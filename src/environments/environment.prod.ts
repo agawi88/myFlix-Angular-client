310a78f1c20067ae2931b239382c0dce491e4a30
Agnieszka <agnessn@icloud.com>
2025-12-15 14:54:19 +0100

    edited files
    changed the prod URL and deleted it from gitignore for a moment

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  // Production API URL - replace with your deployed backend
+ // apiUrl: 'https://british-movies-23cb3bbeb9f8.herokuapp.com'
+ apiUrl: 'https://gbmoviesflix.netlify.app'
+};
