ee80e597ab6f850b8095ecde8cca963ece694c08
Agnieszka <agnessn@icloud.com>
2025-12-19 21:15:41 +0100

    updated env file
    added correct heroku link

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // Production API URL - replace with your deployed backend
- apiUrl: 'https://british-movies-23cb3bbeb9f8.herokuapp.com'
+ apiUrl: 'https://gb-movies-api-cab43a70da98.herokuapp.com/'
 };
