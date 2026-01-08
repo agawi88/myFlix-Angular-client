a9e4ab7d2f30102301e4ddce6014956754c95194
Agnieszka <agnessn@icloud.com>
2026-01-08 17:50:13 +0100

    files created while deploying my app

---
+++ .angular/cache 2/20.3.13/myFlix-Angular-client/vite/deps/@angular_cdk_text-field.js
@@ -0,0 +1,20 @@
+import {
+  AutofillMonitor,
+  CdkAutofill,
+  CdkTextareaAutosize,
+  TextFieldModule
+} from "./chunk-VCZ2DJ6T.js";
+import "./chunk-LB4VAS7C.js";
+import "./chunk-UO3VZI37.js";
+import "./chunk-APPCZKFW.js";
+import "./chunk-KVLC47DY.js";
+import "./chunk-HWYXSU2G.js";
+import "./chunk-JRFR6BLO.js";
+import "./chunk-MARUHEWW.js";
+import "./chunk-WDMUDEB6.js";
+export {
+  AutofillMonitor,
+  CdkAutofill,
+  CdkTextareaAutosize,
+  TextFieldModule
+};
